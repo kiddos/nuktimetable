b75fc430d57e60e701cb875ae5e6b203e2dd239a
Kiddos <josephyu831007@yahoo.com.tw>
2015-08-31 09:41:57 +0800

    add some details

--- app/src/main/java/com/kiddos/nuktimetable/MainActivity.java
+++ app/src/main/java/com/kiddos/nuktimetable/MainActivity.java
@@ -1,21 +1,21 @@
 package com.kiddos.nuktimetable;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.*;
+import android.os.*;
+import android.view.*;
 
 public class MainActivity extends Activity implements OnLoginListener {
 	private Fragment settingFragment;
 	private Fragment mainFragment;
-	private boolean hideMenu = true;
+	private Handler handler;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		handler = new Handler();
+		setDisplayActionBar(false);
 
 		FragmentManager manager = this.getFragmentManager();
 		settingFragment = new LoginFragment();
@@ -23,23 +23,31 @@ public class MainActivity extends Activity implements OnLoginListener {
 		manager.beginTransaction().add(R.id.container, settingFragment).commit();
 	}
 
+	private void setDisplayActionBar(final boolean display) {
+		handler.post(new Runnable() {
+			@Override
+			public void run() {
+				ActionBar actionBar = getActionBar();
+				if (actionBar != null) {
+					if (display) actionBar.show();
+					else actionBar.hide();
+				}
+			}
+		});
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu_main, menu);
-
-		if (hideMenu) {
-			for (int i = 0 ; i < menu.size() ; i ++) {
-				menu.getItem(i).setVisible(false);
-			}
-		}
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_logout) {
+			setDisplayActionBar(false);
+			getFragmentManager().beginTransaction().replace(R.id.container, settingFragment).commit();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -47,14 +55,12 @@ public class MainActivity extends Activity implements OnLoginListener {
 
 	@Override
 	public void onLogin(String webContent) {
+		// display the action bar
+		setDisplayActionBar(true);
+		// change to main fragment
 		Bundle arg = new Bundle();
 		arg.putString(MainFragment.KEY_CONTENT, webContent);
 		mainFragment.setArguments(arg);
-
 		getFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
-
-		// display the menu
-		hideMenu = false;
-		invalidateOptionsMenu();
 	}
 }
