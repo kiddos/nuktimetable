a0a50c867ab7d5cc343c200251abe3f49d0a1840
Kiddos <josephyu831007@yahoo.com.tw>
2015-09-02 11:55:21 +0800

    use gridview design

--- app/src/main/java/com/kiddos/nuktimetable/MainFragment.java
+++ app/src/main/java/com/kiddos/nuktimetable/MainFragment.java
@@ -3,58 +3,55 @@ package com.kiddos.nuktimetable;
 import android.app.*;
 import android.content.Context;
 import android.os.*;
-import android.util.Log;
 import android.view.*;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.TextView;
+import android.widget.*;
 
 public class MainFragment extends Fragment {
 	public static final String KEY_CONTENT = "content";
-	private GridViewAdapter adapter;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-//		TextView test = (TextView) rootView.findViewById(R.id.test);
-//		final GridView grid = (GridView) rootView.findViewById(R.id.gvSchedule);
-//		adapter = new GridViewAdapter(getActivity());
-//		grid.setAdapter(adapter);
+		GridView schedule = (GridView) rootView.findViewById(R.id.gvSchedule);
+		schedule.setAdapter(new ScheduleAdpater(getActivity()));
 
+//		TextView test = (TextView) rootView.findViewById(R.id.test);
 //		Bundle arg = getArguments();
 //		String webContent = arg.getString(KEY_CONTENT);
 //		test.setText(webContent);
 		return rootView;
 	}
 
-	private class GridViewAdapter extends BaseAdapter {
+	private class ScheduleAdpater extends BaseAdapter {
 		private Context context;
-		private int[] layouts = {
+		private int[] layoutId = {
 			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
-				R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item,
+			R.layout.block_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item, R.layout.class_item
 		};
-		public GridViewAdapter(Context context) {
+
+		public ScheduleAdpater(Context context) {
 			this.context = context;
 		}
 
 		@Override
 		public int getCount() {
-			return layouts.length;
+			return 6 * 15;
 		}
 
 		@Override
@@ -69,23 +66,9 @@ public class MainFragment extends Fragment {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			Log.i("Position", position + "");
-			if (convertView == null) {
-				convertView = LayoutInflater.from(context).inflate(layouts[position], parent, false);
-				convertView.setLayoutParams(new GridView.LayoutParams(85, 85));
-			} else if (convertView.getId() != layouts[position]) {
-				convertView = LayoutInflater.from(context).inflate(layouts[position], parent, false);
-				convertView.setLayoutParams(new GridView.LayoutParams(85, 85));
-			}
-			switch (position % 6) {
-				case 0:
-					break;
-				case 1:
-				case 2:
-				case 3:
-				case 4:
-				case 5:
-					break;
+			if (convertView == null || convertView.getId() != layoutId[position]) {
+				LayoutInflater inflater = LayoutInflater.from(context);
+				convertView = inflater.inflate(layoutId[position], parent, false);
 			}
 			return convertView;
 		}
