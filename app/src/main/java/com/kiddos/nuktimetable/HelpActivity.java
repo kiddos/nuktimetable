7d97a535d44e69af5780fe42f79da203be49e802
kiddos <josephyu831007@yahoo.com.tw>
2016-02-26 18:22:09 +0800

    remove the font for help activity

--- app/src/main/java/com/kiddos/nuktimetable/HelpActivity.java
+++ app/src/main/java/com/kiddos/nuktimetable/HelpActivity.java
@@ -12,16 +12,6 @@ public class HelpActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
 
-		final int[] ids = {
-				R.id.tvHelp1, R.id.tvHelp1_1, R.id.tvHelp2,
-				R.id.tvHelp3, R.id.tvHelp4, R.id.tvHelp5,
-				R.id.help_title, R.id.btnUnderstand
-		};
-		final Typeface typeface = Typeface.createFromAsset(getAssets(), "custom_font.ttf");
-		for (int id : ids) {
-			((TextView) findViewById(id)).setTypeface(typeface);
-		}
-
 		findViewById(R.id.btnUnderstand).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
